7e3c4deb1bc24580c65777c993b41294dbfa6cc1
Arthur <arthurmoulin@hotmail.fr>
2018-02-04 19:27:10 +0000

    Add style with semantic UI, rename basket by contracts, add menu

--- app.js
+++ app.js
@@ -16,13 +16,61 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Tells method-override what pattern in the url it should look for
 app.use(methodOverride("_method"));
 
+////////////////
+///   DATA   ///
+////////////////
+
+var products = [
+    {
+        name: "Carrot",
+        img: "https://images.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.Z6S0wQ81H7iNGsH9CjqjFwHaE8%26pid%3D15.1&f=1",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et dolore labore aspernatur itaque nobis dignissimos maxime praesentium, quam vero sit facilis, perspiciatis recusandae explicabo assumenda sed ipsam blanditiis doloribus saepe!"
+    },
+    {
+        name: "Honey",
+        img: "https://images.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.MrJ7a2lttwi7vvgxJOlq9wHaHa%26pid%3D15.1&f=1",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et dolore labore aspernatur itaque nobis dignissimos maxime praesentium, quam vero sit facilis, perspiciatis recusandae explicabo assumenda sed ipsam blanditiis doloribus saepe!"
+    },
+    {
+        name: "Cheese",
+        img: "https://images.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.G9y5uy7QcRh7e0pmz5j6hgHaFj%26pid%3D15.1&f=1",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et dolore labore aspernatur itaque nobis dignissimos maxime praesentium, quam vero sit facilis, perspiciatis recusandae explicabo assumenda sed ipsam blanditiis doloribus saepe!"
+    },
+    {
+        name: "Carrot",
+        img: "https://images.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.Z6S0wQ81H7iNGsH9CjqjFwHaE8%26pid%3D15.1&f=1",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et dolore labore aspernatur itaque nobis dignissimos maxime praesentium, quam vero sit facilis, perspiciatis recusandae explicabo assumenda sed ipsam blanditiis doloribus saepe!"
+    },
+    {
+        name: "Honey",
+        img: "https://images.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.MrJ7a2lttwi7vvgxJOlq9wHaHa%26pid%3D15.1&f=1",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et dolore labore aspernatur itaque nobis dignissimos maxime praesentium, quam vero sit facilis, perspiciatis recusandae explicabo assumenda sed ipsam blanditiis doloribus saepe!"
+    },
+    {
+        name: "Cheese",
+        img: "https://images.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.G9y5uy7QcRh7e0pmz5j6hgHaFj%26pid%3D15.1&f=1",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et dolore labore aspernatur itaque nobis dignissimos maxime praesentium, quam vero sit facilis, perspiciatis recusandae explicabo assumenda sed ipsam blanditiis doloribus saepe!"
+    }
+];
+
 
 //////////////////
 ///   ROUTES   ///
 //////////////////
-var indexRoutes = require("./routes/index");
+//var indexRoutes = require("./routes/index");
+//app.use(indexRoutes);
 
-app.use(indexRoutes);
+app.get("/", function(req, res) {
+    res.render("home");
+});
+
+app.get("/basket", function(req, res) {
+    res.render("basket");
+});
+
+app.get("/bulk", function(req, res) {
+    res.render("bulk", { products: products });
+});
 
 
 ////////////////////////
