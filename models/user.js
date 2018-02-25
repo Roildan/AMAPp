c9b1d8d93e2dae73c5916d67860349474b2ac266
Arthur <arthurmoulin@hotmail.fr>
2018-02-25 18:45:51 +0000

    Add personnal information for users (can be view and and edit on profile page)

--- models/user.js
+++ models/user.js
@@ -2,15 +2,31 @@ const mongoose = require("mongoose"),
     passportLocalMongoose = require("passport-local-mongoose");
 
 const userSchema = new mongoose.Schema({
+    // Main
     username: String,
     password: String,
     email: String,
+    isProducer: { type: Boolean, default: false },
+    isAdmin: { type: Boolean, default: false },
+
+    // Personnal
+    name: {
+        firstName: String,
+        lastName: String
+    },
+    contact: {
+        phone: String,
+        address1: String,
+        address2: String,
+        postalCode: String,
+        city: String
+    },
+
+    // Contract
     subscribedContracts: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: "Contract"
-    }],
-    isProducer: { type: Boolean, default: false },
-    isAdmin: { type: Boolean, default: false }
+    }]
 });
 
 userSchema.plugin(passportLocalMongoose);
--- routes/users.js
+++ routes/users.js
@@ -9,6 +9,54 @@ router.get("/profile", middleWare.isLoggedIn, function(req, res) {
     res.render("users/profile");
 });
 
+router.get("/profile/edit", middleWare.isLoggedIn, function(req, res) {
+    res.render("users/editProfile");
+});
+
+router.put("/profile", middleWare.isLoggedIn, function(req, res) {
+    req.checkBody("email", "Enter a valid email address").isEmail();
+    req.checkBody("contact[phone]", "Enter a valid phone number").isMobilePhone("fr-FR");
+    req.checkBody("contact[postalCode]", "Enter a valid postal code").isPostalCode("FR");
+    let inputErrors = req.validationErrors();
+    if (inputErrors) {
+        let inputErrorsStr = "";
+        for (let i = 0; i < inputErrors.length; i++) {
+            inputErrorsStr += inputErrors[i].msg + "\n";
+        }
+        // Remove last '\n'
+        inputErrorsStr = inputErrorsStr.slice(0, -1);
+        req.flash(
+            "error",
+            "Edit profile error\n" + inputErrorsStr
+        );
+        return res.redirect("/" + req.user.username + "/profile/edit");
+    }
+
+    const updatedUser = req.user;
+    updatedUser.email = req.body.email;
+    updatedUser.name = req.body.name;
+    updatedUser.contact = req.body.contact;
+    updatedUser.contact.city = updatedUser.contact.city.toUpperCase();
+
+    User.findByIdAndUpdate(req.user._id, updatedUser, function(err, user) {
+        if (err) {
+            console.log(err);
+            req.flash(
+                "error",
+                "You cannot be found\nAn error has occurred finding you, please contact an admin for more info"
+            );
+            return res.redirect("back");
+        }
+        else {
+            req.flash(
+                "success",
+                "Your profile have been successfully updated !\nPlease check if you haven't done any typo error"
+            );
+            res.redirect("/" + user.username + "/profile");
+        }
+    });
+});
+
 router.get("/settings", middleWare.isLoggedIn, function(req, res) {
     res.render("users/settings");
 });
