9fa65d9e749b3007eed392fa18bdd6ea278c8326
Arthur <arthurmoulin@hotmail.fr>
2018-03-04 05:57:59 +0000

    First implementation of planning : table creation and fill OK, but don't use frequency nor date

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require("express"),
     router = express.Router();
 
 const User = require("../models/user"),
+    Address = require("../models/address"),
     middleWare = require("../middleware");
 
 const tools = require("./tools");
@@ -75,7 +76,77 @@ router.get("/my_contracts", middleWare.isLoggedIn, function(req, res) {
 });
 
 router.get("/planning", middleWare.isLoggedIn, function(req, res) {
-    res.render("users/planning");
+    User
+        .findById(req.user._id)
+        .populate("contract.subscribed")
+        .exec(function(err, user) {
+            if (err || !user) {
+                console.log(err);
+                req.flash(
+                    "error",
+                    "You cannot be found\nAn error has occurred finding you, please contact an admin for more info"
+                );
+                return res.redirect("back");
+            }
+
+            // Manually populate address in subscribed contracts
+            Address.find(function(err, addresses) {
+                if (err) {
+                    console.log(err);
+                    req.flash(
+                        "error",
+                        "This addresses cannot be found\nAn error has occurred finding the addresses, please contact an admin for more info"
+                    );
+                    return res.redirect("back");
+                }
+
+                user.contract.subscribed.forEach(function(contract) {
+                    addresses.forEach(function(address) {
+                        if (contract.delivery.address.equals(address._id)) {
+                            contract.delivery.address = address;
+                        }
+                    });
+                });
+
+                const days = [
+                    [],
+                    [],
+                    [],
+                    [],
+                    [],
+                    [],
+                    [],
+                ];
+                user.contract.subscribed.forEach(function(contract) {
+                    switch (contract.delivery.dayOfWeek) {
+                        case "Monday":
+                            days[0].push(contract);
+                            break;
+                        case "Tuesday":
+                            days[1].push(contract);
+                            break;
+                        case "Wednesday":
+                            days[2].push(contract);
+                            break;
+                        case "Thursday":
+                            days[3].push(contract);
+                            break;
+                        case "Friday":
+                            days[4].push(contract);
+                            break;
+                        case "Saturday":
+                            days[5].push(contract);
+                            break;
+                        case "Sunday":
+                            days[6].push(contract);
+                            break;
+                    }
+                });
+                const maxPerDay = Math.max(...days.map(tab => tab.length));
+
+                res.render("users/planning", { days: days, maxPerDay: maxPerDay });
+            });
+        });
 });
 
 router.get("/management", middleWare.isLoggedIn, middleWare.isAdmin, function(req, res) {
